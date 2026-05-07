c3d7696cd1e846e73b9caeedaaf9f075de1f5644
Maksym Lunin <nut.code.monkey@gmail.com>
2026-05-07 15:53:05 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mcp-swift-hummingbird",
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "mcp-swift-hummingbird"
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Sources/mcp-swift-hummingbird/mcp_swift_hummingbird.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct mcp_swift_hummingbird {
+    static func main() {
+        print("Hello, world!")
+    }
+}
